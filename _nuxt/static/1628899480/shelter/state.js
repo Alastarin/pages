3c35a1f47f50fd9310ac30cbae853395793cb7dc
Alastarin <36680963+Alastarin@users.noreply.github.com>
2021-08-14 00:04:45 +0000

    Deploying to main from @ Alastarin/warframe-helper-frontend@cee16793dde42783de1d9a1377fb0a50117b6882 🚀

---
+++ _nuxt/static/1628899480/shelter/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpages\u002F_nuxt\u002Fstatic\u002F1628899480",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fshelter",config:{_app:{basePath:"\u002Fpages\u002F",assetsPath:"\u002Fpages\u002F_nuxt\u002F",cdnURL:a}}}}(null));
