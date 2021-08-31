206af9e52fee2fe7849800e20094af326a8c7565
Alastarin <36680963+Alastarin@users.noreply.github.com>
2021-08-31 18:23:43 +0000

    Deploying to main from @ Alastarin/warframe-helper-frontend@ac058684280f067490154d2240ad2faf20174480 🚀

--- _nuxt/static/1629849415/messages/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpages\u002F_nuxt\u002Fstatic\u002F1629849415",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fmessages",config:{_app:{basePath:"\u002Fpages\u002F",assetsPath:"\u002Fpages\u002F_nuxt\u002F",cdnURL:a}}}}(null));
